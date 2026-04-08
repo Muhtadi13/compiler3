365fc4a13b14cbbcb8670206e92b702312d807bc
Al Muhit Muhtadi <113031079+Muhtadi13@users.noreply.github.com>
2026-04-08 18:38:31 +0600

    Modify runner.sh for improved build and cleanup
    Updated the build commands to include specific output files and added force removal for cleanup.

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 yacc --yacc -Wcounterexample -d 2005013.y -o y.tab.cpp
 flex -o 2005013.cpp 2005013.l
-g++ -w *.
-rm 2005013.cpp y.tab.cpp y.tab.hpp
+g++ -w y.tab.cpp 2005013.cpp
+rm -f 2005013.cpp y.tab.cpp y.tab.hpp
 ./a.out input.txt
